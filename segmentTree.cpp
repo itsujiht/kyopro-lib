eef40e98fe7ad2fce755608aca91cb1c734d0b56
nemusheep <miyomayo02@gmail.com>
2026-01-05 05:14:19 +0900

    modify segtree

--- segmentTree.cpp
+++ segmentTree.cpp
@@ -1,44 +1,54 @@
 template <typename T>
 class segmenttree{
     public:
-    vector<T> date1;
-    vector<T> date2;
+    vector<T> mx;
+    vector<T> mi;
     const T INF = numeric_limits<T>::max();
+    const T NINF = numeric_limits<T>::lowest();
     int sz;
 
     void init(int n){
         sz = 1;
         while(sz < n) sz <<= 1;
-        date1.resize(sz * 2, 0);
-        date2.resize(sz * 2, INF);
+        mx.resize(sz * 2, NINF);
+        mi.resize(sz * 2, INF);
     }
 
     void update(int pos, T value){
         pos += sz;
-        date1[pos] = max(date1[pos], value);
-        date2[pos] = min(date2[pos], value);
+        mx[pos] = value;
+        mi[pos] = value;
         while(pos > 1){
             pos >>= 1;
-            date1[pos] = max(date1[pos * 2], date1[pos * 2 + 1]);
-            date2[pos] = min(date2[pos * 2], date2[pos * 2 + 1]);
+            mx[pos] = max(mx[pos * 2], mx[pos * 2 + 1]);
+            mi[pos] = min(mi[pos * 2], mi[pos * 2 + 1]);
         }
     }
 
-    T query1(int left, int right, int open, int close, int now){
-        if(right <= open || close <= left) return 0;
-        if(left <= open && close <= right) return date1[now];
+    T mx_query(int left, int right){
+        return mx_query(left, right, 0, sz, 1);
+    }
+
+    T mi_query(int left, int right){
+        return mi_query(left, right, 0, sz, 1);
+    }
+
+    private:
+    T mx_query(int left, int right, int open, int close, int now){
+        if(right <= open || close <= left) return NINF;
+        if(left <= open && close <= right) return mx[now];
         int middle = (open + close) / 2;
-        T lval = query1(left, right, open, middle, now * 2);
-        T rval = query1(left, right, middle, close, now * 2 + 1);
+        T lval = mx_query(left, right, open, middle, now * 2);
+        T rval = mx_query(left, right, middle, close, now * 2 + 1);
         return max(lval, rval);
     }
 
-    T query2(int left, int right, int open, int close, int now){
+    T mi_query(int left, int right, int open, int close, int now){
         if(right <= open || close <= left) return INF;
-        if(left <= open && close <= right) return date2[now];
+        if(left <= open && close <= right) return mi[now];
         int middle = (open + close) / 2;
-        T lval = query2(left, right, open, middle, now * 2);
-        T rval = query2(left, right, middle, close, now * 2 + 1);
+        T lval = mi_query(left, right, open, middle, now * 2);
+        T rval = mi_query(left, right, middle, close, now * 2 + 1);
         return min(lval, rval);
     }
 };
